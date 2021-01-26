95aa4d6e47847f2b75fa9905170f72095162ff9a
Deepender Rathore <deepenrathore28@gmail.com>
2021-01-27 01:21:36 +0530

    Using BeforeMethod & AfterMethod to initialize & quit the driver

--- src/test/java/me/deepender/automation/tests/LoginTest.java
+++ src/test/java/me/deepender/automation/tests/LoginTest.java
@@ -5,24 +5,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
-    @Test
-    public void test1(){
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", FrameworkConstants.getChromeDriverPath());
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("Selenium", Keys.ENTER);
+    }
+
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
     }
+
     @Test
-    public void test2(){
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resources/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+    public void test1() {
+        driver.findElement(By.name("q")).sendKeys("Selenium", Keys.ENTER);
+    }
+
+    @Test
+    public void test2() {
         driver.findElement(By.name("q")).sendKeys("DevOps", Keys.ENTER);
-        driver.quit();
     }
 }
