4dff62d3b56cb6c0dbf8b51b73c07f82785b7299
Deepender Rathore <deepenrathore28@gmail.com>
2021-01-27 00:59:01 +0530

    Adding dummy test + testng.xml

---
+++ src/test/java/me/deepender/automation/tests/LoginTest.java
@@ -0,0 +1,27 @@
+package me.deepender.automation.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void test1(){
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("Selenium", Keys.ENTER);
+        driver.quit();
+    }
+    @Test
+    public void test2(){
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("DevOps", Keys.ENTER);
+        driver.quit();
+    }
+}
