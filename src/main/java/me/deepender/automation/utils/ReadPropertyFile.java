8b9a82990ad4f555bf67bfcbef32820414bab037
Deepender Rathore <deepenrathore28@gmail.com>
2021-02-06 00:41:03 +0530

    Loading the config file eagerly usubg statuc block

--- src/main/java/me/deepender/automation/utils/ReadPropertyFile.java
+++ src/main/java/me/deepender/automation/utils/ReadPropertyFile.java
@@ -1,6 +1,8 @@
 package me.deepender.automation.utils;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -9,15 +11,23 @@ public class ReadPropertyFile {
 
     }
 
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/config/config.properties");
+            properties.load(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static String getValue(String key) throws Exception {
-        String value = "";
-        Properties properties = new Properties();
-        FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/config/config.properties");
-        properties.load(file);
-        value = properties.getProperty(key);
-        if (Objects.isNull(value)) {
+        if (Objects.isNull(properties.getProperty(key)) || Objects.isNull(key)) {
             throw new Exception("Property name " + key + " is not valid. Please check the config.properties");
         }
-        return value;
+        return properties.getProperty(key);
     }
 }
