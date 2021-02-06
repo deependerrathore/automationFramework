0d4a6dca8bfbf16a5483223d9c597b2c59042f70
Deepender Rathore <deepenrathore28@gmail.com>
2021-02-06 21:11:41 +0530

    Adding AssertJ assertion

--- src/test/java/me/deepender/automation/tests/HomePageTest.java
+++ src/test/java/me/deepender/automation/tests/HomePageTest.java
@@ -1,9 +1,13 @@
 package me.deepender.automation.tests;
 
 import me.deepender.automation.driver.DriverManager;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public final class HomePageTest extends BaseTest {
 
@@ -11,9 +15,29 @@ public final class HomePageTest extends BaseTest {
 
     }
 
+    /**
+     * Validate whether the title is containing Google Search or google search
+     * Validate whether the title is not null and the lenght of title is greater than 15 and less than 100
+     * Check for the links in the pages --> DevOps - Youtube
+     * number of links displayed is exactly 19
+     */
     @Test
     public void test2() {
         DriverManager.getDriver().findElement(By.name("q")).sendKeys("DevOps", Keys.ENTER);
+        String title = DriverManager.getDriver().getTitle();
+
+        Assertions.assertThat(title)
+                .as("Title is not matching").containsIgnoringCase("Google Search")
+                .isNotNull()
+                .hasSizeBetween(15,100)
+                .matches("\\w.*" +" Google Search");
+
+        List<WebElement> elementList = DriverManager.getDriver().findElements(By.xpath("//h3/span"));
+
+        Assertions.assertThat(elementList)
+                .hasSize(19)
+                .extracting(e->e.getText())
+                .contains("DevOps - Wikipedia");
     }
 
 }
